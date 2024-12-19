b7eb1f3188314e7c0dee9803577a339eb5aab1d1
taha <taha.marey100@gmail.com>
2024-12-19 16:40:33 +0300

    finshed dashboard theme

---
+++ resources/views/dashboard/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="rtl">
+
+@include('dashboard.parts.head')
+
+<body class="vertical-layout vertical-menu-modern 2-columns   menu-expanded fixed-navbar" data-open="click"
+    data-menu="vertical-menu-modern" data-col="2-columns">
+    <!-- fixed-top-->
+        @include('dashboard.parts.navbar')
+    <!-- ////////////////////////////////////////////////////////////////////////////-->
+        @include('dashboard.parts.sidebar')
+        @yield('content')
+    <!-- ////////////////////////////////////////////////////////////////////////////-->
+    @include('dashboard.parts.footer')
+    @include('dashboard.parts.scripts')
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+    ], function(){ //...
+        Route::get('test',function(){
+            // return "taha";
+            return view('dashboard.index');
+        });
+    });
+
